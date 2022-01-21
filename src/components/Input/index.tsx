eb236abc9b2de7496eceab6e34c23d0dbaa68e66
agustinettig <40875912+agustinettig@users.noreply.github.com>
2022-01-21 20:57:46 -0300

    convert to typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,13 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends React.HTMLProps<HTMLInputElement> {
+  name: string,
+  icon?: React.FC<{size: number}>,
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
